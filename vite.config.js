4f9608eafcf2fc450c5e9c4b2f9465bbd4eb6da6
Melkamu Tessema <47281626+melkamu372@users.noreply.github.com>
2023-09-13 11:08:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'./vite-deploy/'
+  base:'./vite-deploy/',
+  server: {
+    hmr: {
+      protocol: 'wss',
+      mimeTypes: {
+        'application/javascript': ['js', 'mjs'],
+        'text/javascript': ['jsx'],
+      },
+    },
+  },
+
 })
