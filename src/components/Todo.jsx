3e68bee186974354e5281cf8f9f3062b3f64438a
melkamu372 <melkamu372@gmail.com>
2023-09-18 13:28:49 +0300

    add react testing

---
+++ src/components/Todo.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+class ClassInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+      inputVal: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState((state) => ({
+      ...state,
+      inputVal: e.target.value,
+    }));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState((state) => ({
+      todos: state.todos.concat(state.inputVal),
+      inputVal: "",
+    }));
+  }
+
+  render() {
+    return (
+      <section>
+        <h3>{this.props.name}</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="task-entry">Enter a task: </label>
+          <input
+            type="text"
+            name="task-entry"
+            value={this.state.inputVal}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <h4>All the tasks!</h4>
+        <ul>
+          {this.state.todos.map((todo) => (
+            <li key={todo}>{todo}</li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default ClassInput;
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './vite-deploy/',
-  server: {
-    hmr: {
-      protocol: 'wss',
-      mimeTypes: {
-        'application/javascript': ['js', 'mjs', 'jsx'], // Include 'jsx' in the 'application/javascript' MIME type
-      },
-    },
-  },
+  base: '/React-Application-Practice/',
 })
