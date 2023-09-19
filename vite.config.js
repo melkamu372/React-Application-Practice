89f40cb435a5b9324b52fd03b832e254ce8ee5fd
melkamu372 <melkamu372@gmail.com>
2023-09-19 10:58:22 +0300

    add vitest setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [react()],
   base: '/React-Application-Practice/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
 })
