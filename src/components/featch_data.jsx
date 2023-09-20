7391d559b854e5337af5fec7b190e25f9323c7ae
melkamu372 <melkamu372@gmail.com>
2023-09-20 15:59:26 +0300

    featch the data from the api

--- src/components/featch_data.jsx
+++ src/components/featch_data.jsx
@@ -1,29 +1,37 @@
-import { useEffect, useState } from "react";                                
+import { useState, useEffect } from "react";
+
+const useImageURL = () => {
+  const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setImageURL(response[0].url))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { imageURL, error, loading };
+};
+
 const Image = () => {
-    const [imageURL, setImageURL] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-      fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
-        .then((response) => {
-          if (response.status >= 400) {
-            throw new Error("server error");
-          }
-          return response.json();
-        })
-        .then((response) => setImageURL(response[0].url))
-        .catch((error) => setError(error))
-        .finally(() => setLoading(false));
-    }, []);
-  
-    if (error) return <p>A network error was encountered</p>;
-    if (loading) return <p>Loading...</p>;
-  
-    return (
-      <>
-        <h1>An image</h1>
-        <img src={imageURL} alt={"placeholder text"} />
-      </>
-    );
-  };
-  export default Image;
+  const { imageURL, error, loading } = useImageURL();
+
+  if (error) return <p>A network error was encountered</p>;
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <h1>An image</h1>
+      <img src={imageURL} alt={"placeholder text"} />
+    </>
+  );
+};
+export default Image
