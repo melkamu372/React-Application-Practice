e1c8361b308c0b7ea0cfddf3e4b07493223c0347
melkamu372 <melkamu372@gmail.com>
2023-10-05 13:54:20 +0300

    add node server

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/React-Application-Practice',
+  esbuild: {
+    target: 'es2020',
+    legalComments: 'inline',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
